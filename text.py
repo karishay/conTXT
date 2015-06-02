3020a35d8a74a36a371669da2592fc10393f7f50
karishay <kari.shay@gmail.com>
2015-06-01 20:40:27 -0700

    refactored route to recieve and process sms, added/updated json display routes

--- text.py
+++ text.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 from twilio.rest import TwilioRestClient
 import twilio.twiml
 
@@ -7,17 +7,44 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config.from_pyfile('./config.py')
 
-dict_of_responses = []
+
+dict_of_responses = [{"True": {"feb 3rd, 2015": [1,2,3,4,5,3,4,3]}}, {"False":[{"april 18th, 2015": [1,2,3,3,2,1]}]}]
 
 @app.route('/recieve_data', methods=["GET","POST"])
 def recieve_data():
-    """Recieves incoming text data"""
-    sms_body = request.values.get("Body")
-    dict_of_responses.append(sms_body)
+    """Recieves incoming text data, if "True" is not None, add to list"""
+    if dict_of_responses[0]["True"] != None:
+        sms_body = request.values.get("Body")
+        #get the name of the recording
+        recording_name = dict_of_responses[0]["True"].keys()[0]
+        #look up by name of recording and add the sms to the list
+        dict_of_responses[0]["True"][recording_name].append(sms_body)
     resp = twilio.twiml.Response()
     resp.message()
     print dict_of_responses
     return str(resp)
+
+@app.route('/list_of_recordings')
+def list_of_recordings():
+    """ Returns all names of recordings and which ones are active"""
+    #return render_template(jsonified dict_of_responses)
+    return render_template("queued_list.json")
+
+@app.route('/start_recording/<name_of_recording>')
+def start_recording(name_of_recording):
+    """ Given a name, check to see if there's a recording in the dict
+    with that name, if so, set to active, if not, move current active to
+    false and set new name to active """
+    #if name_of_recording == key in dict_of_responses[1]
+        #add
+    #return render_template("start_recording.json", recording_name=name_of_recording)
+    return "Yay, name of recording is %s" % name_of_recording
+
+@app.route('/stop_recording/<name_of_recording>')
+def stop_recording(name_of_recording):
+    """ Given a name of a recording, if that name is in "True", move it to
+    "False" and set "True" to None, if it is not in "True", return an error """
+    return "You wanna stop recording on %s" % name_of_recording
 
 
 if __name__ == '__main__':
